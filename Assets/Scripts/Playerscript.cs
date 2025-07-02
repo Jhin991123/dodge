12c1aa08082946c5449ecc6e4168b5514713e5f1
DESKTOP-SI2O1BD\202_02 <tlsqkrtk99@gmail.com>
2025-07-02 10:48:07 +0900

    7/2 닷지 플레이어 이동

---
+++ Assets/Scripts/Playerscript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Playerscript : MonoBehaviour
+{
+    public Rigidbody playerRigidbody; // Reference to the player's Rigidbody component
+    public float speed = 10f; // Speed of the player movement
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizontal_Speed = Input.GetAxisRaw("Horizontal");
+        float Vertical_Speed = Input.GetAxisRaw("Vertical");
+
+        Vector3 velocity = new Vector3(Horizontal_Speed, 0.0f, Vertical_Speed).normalized * speed;
+        //velocity = Vector3.ClampMagnitude(velocity, m_speed);
+        playerRigidbody.linearVelocity = velocity;
+    }
+}
